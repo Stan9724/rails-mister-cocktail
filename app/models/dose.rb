769ec2539115a68780deaa1b3960974561aefbba
Tristan Boizard <boiz.tristan@gmail.com>
2019-05-17 13:16:01 +0200

    deletedone

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,7 +1,6 @@
 class Dose < ApplicationRecord
-  belongs_to :ingredient
   belongs_to :cocktail
+  belongs_to :ingredient
 
-  validates :description, presence: true
-  validates :cocktail_id, uniqueness: { scope: :ingredient_id }
+  validates :cocktail_id, uniqueness: { scope: :ingredient }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,22 @@
-Cocktail.destroy_all
+require 'json'
+require 'open-uri'
+
 Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
 
-cocktails = [
-  {
-    name: 'Pina Colada'
-  },
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
 
-  {
-    name: 'Colonel'
-  },
+ingredients["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
 
-  {
-    name: 'Planteur'
-  },
+url = 'http://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic'
+cocktails_serialized = open(url).read
+cocktails = JSON.parse(cocktails_serialized)
 
-  {
-    name: 'Bikini Bottom'
-  },
-
-  {
-    name: 'Bob'
-  },
-
-  {
-    name: 'Bloody Mary'
-  }
-]
-
-ingredients = %w(lemon ice mint leaves)
-ingredients.each { |ingredient| Ingredient.create(name: ingredient) }
-
-cocktails.each { |cocktail| Cocktail.create(cocktail) }
+cocktails["drinks"].each do |cocktail|
+  Cocktail.create(name: cocktail["strDrink"])
+end
